c51e046160889ac85bb8aab2e43bc9fcbcc9a25a
Matheus Gaston <theteusviana@gmail.com>
2024-06-24 21:48:27 -0300

    feat: fetch function apis

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,14 @@
 const apiJson = "http://localhost:3000/conteudos"
 const apiGithub = "https://api.github.com/users/GastonZinhow/repos"
 
+/*
+async function carregarGithub(){
+    const repostaGit = await fetch('https://api.github.com/users/GastonZinhow/repos')
+                                .then(function(res){console.log(res)})
+    return repostaGit;
+}
+carregarGithub();
+*/
+fetch('https://api.github.com/users/GastonZinhow/repos')
+    .then((res) => res.json ())
+    .then(data => console.log(data))
